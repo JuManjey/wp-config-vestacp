aac34fa201df2afa59654ee2b972ad19fa67275c
JuManji <20031287+JuManjey@users.noreply.github.com>
2021-10-05 22:11:09 +0300

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,19 @@
+define( 'DB_NAME', 'NAME_OF_DB.tk' );
+
+/** MySQL database username */
+define( 'DB_USER', 'LOGIN_OF_DB' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'PASSWORD_OF_DB' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+define( 'FS_METHOD','direct' );
+define('WP_TEMP_DIR', ABSPATH . 'wp-content/');
